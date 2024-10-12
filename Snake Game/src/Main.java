efaf7b552db820e31c030495ff06d97c2f42d463
scosme20 <scosmeagostinho@hotmail.com>
2024-10-11 22:00:20 -0300

    first commit

---
+++ Snake Game/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+    }
+}
